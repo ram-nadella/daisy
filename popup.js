1122b3d72d0963ac06c4da8455b439dd54178535
Ram Nadella <rnadella@etsy.com>
2014-05-02 13:28:08 -0400

    Tweaked the show listing logic to go off of date

--- popup.js
+++ popup.js
@@ -1,44 +1,66 @@
-var listingDataJSON = '{"listing_id":179754881,"state":"active","user_id":13017856,"category_id":69194763,"title":"Hanging Glass Vase \/ Blown Glass Wall Vase \/ Polka Dot Glass Flower Vase \/ Colorful Home Decor","description":"** Backordered Item - ships out in 3-4 weeks ** \r\n\r\nHanging Glass Flower Vase - Confetti Series\r\n\r\nThese hand blown glass hanging wall vases add a casual elegance to any space.\r\n\r\nThey can be hung from a metal hook or cord whether it is from the wall, ceiling or outside on your patio.  They are particularly beautiful in groupings and make a unique house warming gift.  They can even be a great wall accent with brightly colored flowers in a bathroom or office space.\r\n\r\n_________________________________________________________\r\n\r\nItem Details:\r\n\r\n\r\nPlease note this item is for one vase.  There are more listed in our store.\r\n\r\nThese vases can be used for flowers, air plants, and small succulents.\r\n\r\nWe have created the hanging loop on this vase within the form of the vase itself so it is much more durable than attaching a separate loop or a bit of glass.\r\n\r\nApproximate size: 6&quot; h x 2.5&quot; d x 3.25&quot;w\r\n\r\nItems are hand blown and dimensions can vary by approximately an inch\r\n*The photo images are very accurate and representative of what the item will look like.\r\n\r\n\r\n**Photography courtesy of the amazing Elizabeth Bryant you can visit her shop on Etsy here: \r\nhttp:\/\/www.etsy.com\/shop\/elizaville\r\n__________________________________________________________\r\n\r\n\r\nVisit our store here to view our wide selection of other items:\r\nhttp:\/\/www.etsy.com\/shop\/AvolieGlass?ref=pr_shop_more\r\n\r\nWe DO ship internationally. Please convo us with your address and zip-code and we can send you a quote.\r\n\r\nWe are happy to provide Express Shipping. Please convo us for a quote.","creation_tsz":1398977835,"ending_tsz":1409605035,"original_creation_tsz":1392673760,"last_modified_tsz":1398977835,"price":"50.00","currency_code":"USD","quantity":4,"tags":["terrarium","glass vase","gifts for mom","wall vase","air plant holder","glass","flower vase","hand blown glass","tiny terrariums","glass terrariums","planter","hanging glass vase","hanging vase"],"category_path":["Glass","Home Decor","Vase"],"category_path_ids":[69150361,69154849,69194763],"materials":["glass","blue glass","red glass","green glass","yellow glass","orange glass","pink glass","white glass"],"shop_section_id":10284104,"featured_rank":null,"state_tsz":1398710469,"url":"https:\/\/www.etsy.com\/listing\/179754881\/hanging-glass-vase-blown-glass-wall-vase?utm_source=daisy&utm_medium=api&utm_campaign=api","views":5926,"num_favorers":1014,"shipping_template_id":null,"processing_min":10,"processing_max":15,"who_made":"i_did","is_supply":"false","when_made":"2010_2014","is_private":false,"recipient":"unisex_adults","occasion":"mothers_day","style":["Beach","Cottage Chic"],"non_taxable":false,"is_customizable":true,"is_digital":false,"file_data":"","language":"en-US","has_variations":false,"used_manufacturer":false}';
-var listingImageJSON = '{"count":5,"results":[{"listing_image_id":565484709,"hex_code":"7C6459","red":124,"green":100,"blue":89,"hue":19,"saturation":28,"brightness":48,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":1,"url_75x75":"https:\/\/img1.etsystatic.com\/023\/0\/6226096\/il_75x75.565484709_oq9i.jpg","url_170x135":"https:\/\/img1.etsystatic.com\/023\/0\/6226096\/il_170x135.565484709_oq9i.jpg","url_570xN":"https:\/\/img1.etsystatic.com\/023\/0\/6226096\/il_570xN.565484709_oq9i.jpg","url_fullxfull":"https:\/\/img1.etsystatic.com\/023\/0\/6226096\/il_fullxfull.565484709_oq9i.jpg","full_height":1153,"full_width":1500},{"listing_image_id":565485603,"hex_code":"B4A28F","red":180,"green":162,"blue":143,"hue":31,"saturation":20,"brightness":70,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":2,"url_75x75":"https:\/\/img1.etsystatic.com\/022\/0\/6226096\/il_75x75.565485603_3rs5.jpg","url_170x135":"https:\/\/img1.etsystatic.com\/022\/0\/6226096\/il_170x135.565485603_3rs5.jpg","url_570xN":"https:\/\/img1.etsystatic.com\/022\/0\/6226096\/il_570xN.565485603_3rs5.jpg","url_fullxfull":"https:\/\/img1.etsystatic.com\/022\/0\/6226096\/il_fullxfull.565485603_3rs5.jpg","full_height":798,"full_width":1000},{"listing_image_id":565373972,"hex_code":"B5A48B","red":181,"green":164,"blue":139,"hue":36,"saturation":23,"brightness":70,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":3,"url_75x75":"https:\/\/img0.etsystatic.com\/039\/0\/6226096\/il_75x75.565373972_f3at.jpg","url_170x135":"https:\/\/img0.etsystatic.com\/039\/0\/6226096\/il_170x135.565373972_f3at.jpg","url_570xN":"https:\/\/img0.etsystatic.com\/039\/0\/6226096\/il_570xN.565373972_f3at.jpg","url_fullxfull":"https:\/\/img0.etsystatic.com\/039\/0\/6226096\/il_fullxfull.565373972_f3at.jpg","full_height":824,"full_width":1000},{"listing_image_id":565374180,"hex_code":"A69089","red":166,"green":144,"blue":137,"hue":14,"saturation":17,"brightness":65,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":4,"url_75x75":"https:\/\/img0.etsystatic.com\/031\/0\/6226096\/il_75x75.565374180_qum0.jpg","url_170x135":"https:\/\/img0.etsystatic.com\/031\/0\/6226096\/il_170x135.565374180_qum0.jpg","url_570xN":"https:\/\/img0.etsystatic.com\/031\/0\/6226096\/il_570xN.565374180_qum0.jpg","url_fullxfull":"https:\/\/img0.etsystatic.com\/031\/0\/6226096\/il_fullxfull.565374180_qum0.jpg","full_height":1050,"full_width":1500},{"listing_image_id":565485053,"hex_code":"C4B4A0","red":196,"green":180,"blue":160,"hue":33,"saturation":18,"brightness":76,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":5,"url_75x75":"https:\/\/img1.etsystatic.com\/027\/0\/6226096\/il_75x75.565485053_jnpc.jpg","url_170x135":"https:\/\/img1.etsystatic.com\/027\/0\/6226096\/il_170x135.565485053_jnpc.jpg","url_570xN":"https:\/\/img1.etsystatic.com\/027\/0\/6226096\/il_570xN.565485053_jnpc.jpg","url_fullxfull":"https:\/\/img1.etsystatic.com\/027\/0\/6226096\/il_fullxfull.565485053_jnpc.jpg","full_height":885,"full_width":1000}],"params":{"listing_id":"179754881"},"type":"ListingImage","pagination":{}}';
+;(function($, document, window, undefined) {
+    var listingDataJSON = '{"listing_id":179754881,"state":"active","user_id":13017856,"category_id":69194763,"title":"Hanging Glass Vase \/ Blown Glass Wall Vase \/ Polka Dot Glass Flower Vase \/ Colorful Home Decor","description":"** Backordered Item - ships out in 3-4 weeks ** \r\n\r\nHanging Glass Flower Vase - Confetti Series\r\n\r\nThese hand blown glass hanging wall vases add a casual elegance to any space.\r\n\r\nThey can be hung from a metal hook or cord whether it is from the wall, ceiling or outside on your patio.  They are particularly beautiful in groupings and make a unique house warming gift.  They can even be a great wall accent with brightly colored flowers in a bathroom or office space.\r\n\r\n_________________________________________________________\r\n\r\nItem Details:\r\n\r\n\r\nPlease note this item is for one vase.  There are more listed in our store.\r\n\r\nThese vases can be used for flowers, air plants, and small succulents.\r\n\r\nWe have created the hanging loop on this vase within the form of the vase itself so it is much more durable than attaching a separate loop or a bit of glass.\r\n\r\nApproximate size: 6&quot; h x 2.5&quot; d x 3.25&quot;w\r\n\r\nItems are hand blown and dimensions can vary by approximately an inch\r\n*The photo images are very accurate and representative of what the item will look like.\r\n\r\n\r\n**Photography courtesy of the amazing Elizabeth Bryant you can visit her shop on Etsy here: \r\nhttp:\/\/www.etsy.com\/shop\/elizaville\r\n__________________________________________________________\r\n\r\n\r\nVisit our store here to view our wide selection of other items:\r\nhttp:\/\/www.etsy.com\/shop\/AvolieGlass?ref=pr_shop_more\r\n\r\nWe DO ship internationally. Please convo us with your address and zip-code and we can send you a quote.\r\n\r\nWe are happy to provide Express Shipping. Please convo us for a quote.","creation_tsz":1398977835,"ending_tsz":1409605035,"original_creation_tsz":1392673760,"last_modified_tsz":1398977835,"price":"50.00","currency_code":"USD","quantity":4,"tags":["terrarium","glass vase","gifts for mom","wall vase","air plant holder","glass","flower vase","hand blown glass","tiny terrariums","glass terrariums","planter","hanging glass vase","hanging vase"],"category_path":["Glass","Home Decor","Vase"],"category_path_ids":[69150361,69154849,69194763],"materials":["glass","blue glass","red glass","green glass","yellow glass","orange glass","pink glass","white glass"],"shop_section_id":10284104,"featured_rank":null,"state_tsz":1398710469,"url":"https:\/\/www.etsy.com\/listing\/179754881\/hanging-glass-vase-blown-glass-wall-vase?utm_source=daisy&utm_medium=api&utm_campaign=api","views":5926,"num_favorers":1014,"shipping_template_id":null,"processing_min":10,"processing_max":15,"who_made":"i_did","is_supply":"false","when_made":"2010_2014","is_private":false,"recipient":"unisex_adults","occasion":"mothers_day","style":["Beach","Cottage Chic"],"non_taxable":false,"is_customizable":true,"is_digital":false,"file_data":"","language":"en-US","has_variations":false,"used_manufacturer":false}';
+    var listingImageJSON = '{"count":5,"results":[{"listing_image_id":565484709,"hex_code":"7C6459","red":124,"green":100,"blue":89,"hue":19,"saturation":28,"brightness":48,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":1,"url_75x75":"https:\/\/img1.etsystatic.com\/023\/0\/6226096\/il_75x75.565484709_oq9i.jpg","url_170x135":"https:\/\/img1.etsystatic.com\/023\/0\/6226096\/il_170x135.565484709_oq9i.jpg","url_570xN":"https:\/\/img1.etsystatic.com\/023\/0\/6226096\/il_570xN.565484709_oq9i.jpg","url_fullxfull":"https:\/\/img1.etsystatic.com\/023\/0\/6226096\/il_fullxfull.565484709_oq9i.jpg","full_height":1153,"full_width":1500},{"listing_image_id":565485603,"hex_code":"B4A28F","red":180,"green":162,"blue":143,"hue":31,"saturation":20,"brightness":70,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":2,"url_75x75":"https:\/\/img1.etsystatic.com\/022\/0\/6226096\/il_75x75.565485603_3rs5.jpg","url_170x135":"https:\/\/img1.etsystatic.com\/022\/0\/6226096\/il_170x135.565485603_3rs5.jpg","url_570xN":"https:\/\/img1.etsystatic.com\/022\/0\/6226096\/il_570xN.565485603_3rs5.jpg","url_fullxfull":"https:\/\/img1.etsystatic.com\/022\/0\/6226096\/il_fullxfull.565485603_3rs5.jpg","full_height":798,"full_width":1000},{"listing_image_id":565373972,"hex_code":"B5A48B","red":181,"green":164,"blue":139,"hue":36,"saturation":23,"brightness":70,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":3,"url_75x75":"https:\/\/img0.etsystatic.com\/039\/0\/6226096\/il_75x75.565373972_f3at.jpg","url_170x135":"https:\/\/img0.etsystatic.com\/039\/0\/6226096\/il_170x135.565373972_f3at.jpg","url_570xN":"https:\/\/img0.etsystatic.com\/039\/0\/6226096\/il_570xN.565373972_f3at.jpg","url_fullxfull":"https:\/\/img0.etsystatic.com\/039\/0\/6226096\/il_fullxfull.565373972_f3at.jpg","full_height":824,"full_width":1000},{"listing_image_id":565374180,"hex_code":"A69089","red":166,"green":144,"blue":137,"hue":14,"saturation":17,"brightness":65,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":4,"url_75x75":"https:\/\/img0.etsystatic.com\/031\/0\/6226096\/il_75x75.565374180_qum0.jpg","url_170x135":"https:\/\/img0.etsystatic.com\/031\/0\/6226096\/il_170x135.565374180_qum0.jpg","url_570xN":"https:\/\/img0.etsystatic.com\/031\/0\/6226096\/il_570xN.565374180_qum0.jpg","url_fullxfull":"https:\/\/img0.etsystatic.com\/031\/0\/6226096\/il_fullxfull.565374180_qum0.jpg","full_height":1050,"full_width":1500},{"listing_image_id":565485053,"hex_code":"C4B4A0","red":196,"green":180,"blue":160,"hue":33,"saturation":18,"brightness":76,"is_black_and_white":false,"creation_tsz":1392673760,"listing_id":179754881,"rank":5,"url_75x75":"https:\/\/img1.etsystatic.com\/027\/0\/6226096\/il_75x75.565485053_jnpc.jpg","url_170x135":"https:\/\/img1.etsystatic.com\/027\/0\/6226096\/il_170x135.565485053_jnpc.jpg","url_570xN":"https:\/\/img1.etsystatic.com\/027\/0\/6226096\/il_570xN.565485053_jnpc.jpg","url_fullxfull":"https:\/\/img1.etsystatic.com\/027\/0\/6226096\/il_fullxfull.565485053_jnpc.jpg","full_height":885,"full_width":1000}],"params":{"listing_id":"179754881"},"type":"ListingImage","pagination":{}}';
 
-// var listingData = JSON.parse(listingDataJSON);
+    var listingIDs = ['151071794', '96713551', '97038517', '184344216', '165434922', '183172134'];
 
-// console.log(listingData);
+    var listingData = {
+        '2': {
+            'listing_id': 179754881,
+            'title': 'Hanging Glass Vase / Blown Glass Wall Vase / Polka Dot Glass Flower Vase / Colorful Home Decor',
+            'url': 'https://www.etsy.com/listing/179754881/hanging-glass-vase-blown-glass-wall-vase?utm_source=daisy&utm_medium=api&utm_campaign=api',
+            'num_favorers': 1014,
+            'price': '50.00',
+            'currency_code': 'USD',
+            'images': {
+                'url_170x135': 'https://img1.etsystatic.com/023/0/6226096/il_170x135.565484709_oq9i.jpg',
+                'url_570xN': 'https://img1.etsystatic.com/023/0/6226096/il_570xN.565484709_oq9i.jpg',
+                'url_75x75': 'https://img1.etsystatic.com/023/0/6226096/il_75x75.565484709_oq9i.jpg',
+                'url_fullxfull': 'https://img1.etsystatic.com/023/0/6226096/il_fullxfull.565484709_oq9i.jpg'
+            }
+        },
+        '3': {
+            
+        },
+        '4': {
+            
+        },
+        '5': {
+            
+        },
+        '6': {
+            
+        },
+        '7': {
+            
+        }
+    };
 
-var listingData = {
-	"listing_id": 179754881,
-    "title": "Hanging Glass Vase / Blown Glass Wall Vase / Polka Dot Glass Flower Vase / Colorful Home Decor",
-    "url": "https://www.etsy.com/listing/179754881/hanging-glass-vase-blown-glass-wall-vase?utm_source=daisy&utm_medium=api&utm_campaign=api",
-	"num_favorers": 1014,
-	"price": "50.00",
-	"currency_code": "USD",
-	"images": {
-        "url_170x135": "https://img1.etsystatic.com/023/0/6226096/il_170x135.565484709_oq9i.jpg",
-        "url_570xN": "https://img1.etsystatic.com/023/0/6226096/il_570xN.565484709_oq9i.jpg",
-        "url_75x75": "https://img1.etsystatic.com/023/0/6226096/il_75x75.565484709_oq9i.jpg",
-        "url_fullxfull": "https://img1.etsystatic.com/023/0/6226096/il_fullxfull.565484709_oq9i.jpg"
-	}
-};
+    var showListing = function(data) {
+        var $cart = $('#cart');
+        var $listing = $('#listing');
+        var $price = $('#price');
 
-var $cart = $('#cart');
-var $listing = $('#listing');
-var $price = $('#price');
+        $listing.find('img').attr('src', data.images['url_570xN']);
+        $('#price').text('$ ' + data.price);
 
-var showListing = function(data) {
-	$listing.find('img').attr('src', 'https://img1.etsystatic.com/023/0/6226096/il_570xN.565484709_oq9i.jpg');
-	$('#price').text('$ ' + data.price);
-};
+        $cart.hover(
+            // mouseover
+            function() { $price.fadeIn('slow'); },
+            // mouseout
+            function() { $price.fadeOut(); }
+        );
 
-showListing(listingData);
+        $listing.click(function(e) {
+            window.open(data.url);
+        });
+    };
 
-$cart.hover(function() {
-	$price.fadeIn('slow');
-},
-function() {
-	$price.fadeOut('slow');
-});
+    var today = (new Date()).getDate();
+    if (listingData.hasOwnProperty(today)) {
+        showListing(listingData[today]);
+    } else {
+        console.log('Could not find ' + today + ' in listing data', listingData);
+    }
 
-$listing.click(function(e) {
-	window.open(listingData.url);
-});
-
+})(jQuery, document, window);
